6351c31b3ff810d1f89b4adff826319d9960c51b
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-14 03:21:41 -0400

    added notes

---
+++ research.rb
@@ -0,0 +1,31 @@
+# # current version of ruby
+# # ruby 2.4.1p111 (2017-03-22 revision 58053) [x86_64-linux]
+#
+# string:
+#
+# length - returns the characters length
+#
+# strip - returns a copy of the string with the whitespaced removed.
+#
+# split - divides string into substrings based on a delimiter, returing an array of these strings.
+#
+# start_with? - returns true if string starts with one of the prefixes given
+#
+# Array:
+#
+# first - returns first value of Array
+# delete_at - deletes the index arry.delete_at(0) = deletes first index of Array
+# delete - deletes a particular element anywhere in an array
+# pop - removes the last element from an array
+#
+# Hash:
+# to_a - converts hash to a nested array of [key,value] arrays
+# has_key? - returns true if given key is present in hash
+# has_value? - retuns true if given value is present for some key in Hash
+#
+# Time:
+# now - creates a new time object for the current time
+#
+# File:
+# exist? - returns true if file names exist
+# exstname - retuns the extension portion of the file s
